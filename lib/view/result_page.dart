779bb6dce294e7c1eec800c147c987c8a2b4717a
kuroyei <kroma6317@gmail.com>
2023-01-04 14:28:16 +0900

    ResultPage で正答率に合わせてずんだもんを表示するようにした

--- lib/view/result_page.dart
+++ lib/view/result_page.dart
@@ -5,66 +5,104 @@ class Result extends StatelessWidget {
   int result;
   int questionNumber;
 
+
+
   Future<void> goToTop(BuildContext context) async {
     Navigator.popUntil(context, (route) => route.isFirst);
   }
 
   @override
   Widget build(BuildContext context) {
+    int correctAnswersRate = result ~/ questionNumber * 100;
+    String bgImagePath;
+    if(correctAnswersRate >= 90) {
+      bgImagePath = "bg-result_page-90.png";  
+    } else if(correctAnswersRate >= 80) {
+      bgImagePath = "bg-result_page-80.png";
+    } else if(correctAnswersRate >= 60) {
+      bgImagePath = "bg-result_page-60.png";
+    } else if(correctAnswersRate >= 30) {
+      bgImagePath = "bg-result_page-30.png";
+    } else {
+      bgImagePath = "bg-result_page-00.png";
+    }
+
     return WillPopScope( 
       onWillPop: () async => false,
       child:Scaffold(
         appBar: AppBar(
           title: Text("平家物語ゲーム"),
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.only(bottom: 30),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      "正答数",
-                      style: TextStyle(
-                        fontSize: 40,
-                        fontWeight: FontWeight.bold,
-                      )
+        body: Stack(
+          children: [
+            Container(
+              alignment: Alignment.bottomRight,
+              child: Container(
+                width: MediaQuery.of(context).size.width * 0.5,
+                height: MediaQuery.of(context).size.height * 0.5,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    colorFilter: ColorFilter.mode(
+                      Color(0xFFFAFAFA).withOpacity(0.4),
+                      BlendMode.dstATop
                     ),
-                    Row(
+                    image: AssetImage(bgImagePath),
+                    alignment: Alignment.bottomRight,
+                    fit: BoxFit.contain,
+                  )
+                ),
+              )
+            ),
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(bottom: 30),
+                    child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.end,
                       children: [
                         Text(
-                          "$result",
+                          "正答数",
                           style: TextStyle(
-                            fontSize: 60,
+                            fontSize: 40,
+                            fontWeight: FontWeight.bold,
                           )
                         ),
-                        Padding(
-                          padding: EdgeInsets.only(left: 10),
-                          child: Text(
-                            "/ $questionNumber",
-                            style: TextStyle(
-                              fontSize: 20,
-                            )
-                          ),
-                        ),
-                      ]
-                    )
-                  ],
-                ),
-              ),
-              ElevatedButton(
-                onPressed: () async {
-                  await goToTop(context);
-                },
-                child: const Text('トップへ'),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            Text(
+                              "$result",
+                              style: TextStyle(
+                                fontSize: 60,
+                              )
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(left: 10),
+                              child: Text(
+                                "/ $questionNumber",
+                                style: TextStyle(
+                                  fontSize: 20,
+                                )
+                              ),
+                            ),
+                          ]
+                        )
+                      ],
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () async {
+                      await goToTop(context);
+                    },
+                    child: const Text('トップへ'),
+                  )
+                ],
               )
-            ],
-          )
+            )
+          ],
         )
       )
     );
