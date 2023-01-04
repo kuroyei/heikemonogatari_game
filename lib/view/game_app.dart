381665e757e9006d9b508afffcc9c3fca001fb17
kuroyei <kroma6317@gmail.com>
2023-01-04 11:35:25 +0900

    トップページに GitHub へのリンクを追加

--- lib/view/game_app.dart
+++ lib/view/game_app.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:heikemonogatari_game/view/game_page.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class GameApp extends StatelessWidget {
   GameApp({Key? key}) : super(key: key);
@@ -10,21 +12,43 @@ class GameApp extends StatelessWidget {
       appBar: AppBar(
         title: Text("平家物語ゲーム"),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context, 
-                  MaterialPageRoute(builder: (context) => GamePage())
-                );
+      body: Column(
+        children: [
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context, 
+                      MaterialPageRoute(builder: (context) => GamePage())
+                    );
+                  },
+                  child: const Text("すたーと！"),
+                ), 
+              ],
+            )
+          ),
+          Padding(
+            padding: EdgeInsets.all(30),
+            child: GestureDetector(
+              onTap: () {
+                launchUrl(Uri.parse("https://github.com/kuroyei/heikemonogatari_game"));
               },
-              child: const Text("すたーと！"),
-            ), 
-          ],
-        )
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(FontAwesomeIcons.github),
+                  Padding(
+                    padding: EdgeInsets.only(left: 10),
+                    child: const Text("ぎっとはぶ")
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
       )
     );
   }
