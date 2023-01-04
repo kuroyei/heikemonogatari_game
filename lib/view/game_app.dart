13ff29a402f9b87c5902fa43fdd0e5a51df64d5b
kuroyei <kroma6317@gmail.com>
2023-01-04 13:55:44 +0900

    GameApp の背景にずんだもん

--- lib/view/game_app.dart
+++ lib/view/game_app.dart
@@ -8,48 +8,68 @@ class GameApp extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(
         title: Text("平家物語ゲーム"),
       ),
-      body: Column(
+      body: Stack(
         children: [
-          Expanded(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context, 
-                      MaterialPageRoute(builder: (context) => GamePage())
-                    );
-                  },
-                  child: const Text("すたーと！"),
-                ), 
-              ],
-            )
-          ),
-          Padding(
-            padding: EdgeInsets.all(30),
-            child: GestureDetector(
-              onTap: () {
-                launchUrl(Uri.parse("https://github.com/kuroyei/heikemonogatari_game"));
-              },
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(FontAwesomeIcons.github),
-                  Padding(
-                    padding: EdgeInsets.only(left: 10),
-                    child: const Text("ぎっとはぶ")
-                  )
-                ],
-              ),
+          Container(
+            width: MediaQuery.of(context).size.width * 0.5,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                colorFilter: ColorFilter.mode(
+                  Color(0xFFFAFAFA).withOpacity(0.4),
+                  BlendMode.dstATop
+                ),
+                image: AssetImage("bg-game_app.png"),
+                alignment: Alignment.bottomLeft,
+                fit: BoxFit.contain,
+              )
             ),
           ),
+          Column(
+            children: [
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context, 
+                          MaterialPageRoute(builder: (context) => GamePage())
+                        );
+                      },
+                      child: const Text("すたーと！"),
+                    ), 
+                  ],
+                )
+              ),
+              Padding(
+                padding: EdgeInsets.all(30),
+                child: GestureDetector(
+                  onTap: () {
+                    launchUrl(Uri.parse("https://github.com/kuroyei/heikemonogatari_game"));
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(FontAwesomeIcons.github),
+                      Padding(
+                        padding: EdgeInsets.only(left: 10),
+                        child: const Text("ぎっとはぶ")
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          )
         ],
       )
+      
     );
   }
 }
